e499e1c842c5bd79484b966b37050a1f0c36b48d
Radu Ungureanu <radu.ungureanu@outlook.com>
2023-10-20 10:22:36 +0300

    added source files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # typeform-signature-hash-generator
 Generate the Typform's SignatureHash based on the request's payload
+
+# Publishing the tool
+
+``` bash
+cd src
+dotnet publish .\TypeformSignatureGenerator\TypeformSignatureGenerator.csproj --configuration Release --self-contained true --runtime win-x64 --output ./dist
+```
---
+++ src/TypeformSignatureGenerator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+class Program
+{
+    /// <param name="secret">Secret used to sign the payload</param>
+    /// <param name="filePath">Path to file containing the payload</param>
+    static void Main(string secret, string filePath)
+    {
+        var fileContent = File.ReadAllText(filePath);
+        var hash = CalculateSignature(fileContent, secret);
+
+        Console.WriteLine($"sha256={hash}");
+    }
+
+    private static string CalculateSignature(string text, string secret)
+    {
+        var secretAsBytes = Encoding.UTF8.GetBytes(secret);
+        using var hmacsha256 = new HMACSHA256(secretAsBytes);
+        var textAsBytes = Encoding.UTF8.GetBytes(text);
+        var hash = hmacsha256.ComputeHash(textAsBytes);
+
+        return Convert.ToBase64String(hash);
+    }
+}
